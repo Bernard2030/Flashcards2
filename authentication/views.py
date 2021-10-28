826759b87f492882872a8129ccfbbfd8253e42a5
Bernard Opiyo <brobernard.254@gmail.com>
2021-10-28 15:33:05 +0300

    modify views in auths

--- authentication/views.py
+++ authentication/views.py
@@ -1,16 +1,31 @@
 from django.shortcuts import render
-from rest_framework.generics import GenericAPIView
-from .serializers import RegisterSerializer
+from rest_framework import generics, status, views, permissions
+from .serializers import RegisterSerializer, SetNewPasswordSerializer, ResetPasswordEmailRequestSerializer, EmailVerificationSerializer, LoginSerializer, LogoutSerializer,SubjectSerializer, NotesSerializer
+from rest_framework.views import APIView
+from .permissions import IsAdminOrReadOnly
 from rest_framework.response import Response
-from rest_framework import status,generics
-from .models import User
 from rest_framework_simplejwt.tokens import RefreshToken
+from .models import User,Subject,Notes
+from django.http import Http404
+from .utils import Util
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
+import jwt
+from django.conf import settings
+# from drf_yasg.utils import swagger_auto_schema
+# from drf_yasg import openapi
+# from .renderers import UserRenderer
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.utils.encoding import smart_str, force_str, smart_bytes, DjangoUnicodeDecodeError
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.contrib.sites.shortcuts import get_current_site
 from django.urls import reverse
 from .utils import Util
-
-
-# Create your views here.
+from django.shortcuts import redirect
+from django.http import HttpResponsePermanentRedirect
+import os
+class CustomRedirect(HttpResponsePermanentRedirect):
+    allowed_schemes = [os.environ.get('APP_SCHEME'), 'http', 'https']
 class RegisterView(generics.GenericAPIView):
     serializer_class = RegisterSerializer
     # renderer_classes = (UserRenderer,)
@@ -31,3 +46,152 @@ class RegisterView(generics.GenericAPIView):
                 'email_subject': 'Verify your email'}
         Util.send_email(data)
         return Response(user_data, status=status.HTTP_201_CREATED)
+class VerifyEmail(views.APIView):
+    serializer_class = EmailVerificationSerializer
+    # token_param_config = openapi.Parameter(
+    #     'token', in_=openapi.IN_QUERY, description='Description', type=openapi.TYPE_STRING)
+    # @swagger_auto_schema(manual_parameters=[token_param_config])
+    def get(self, request):
+        token = request.GET.get('token')
+        try:
+            payload = jwt.decode(token, settings.SECRET_KEY)
+            user = User.objects.get(id=payload['user_id'])
+            if not user.is_verified:
+                user.is_verified = True
+                user.save()
+            return Response({'email': 'Successfully activated'}, status=status.HTTP_200_OK)
+        except jwt.ExpiredSignatureError as identifier:
+            return Response({'error': 'Activation Expired'}, status=status.HTTP_400_BAD_REQUEST)
+        except jwt.exceptions.DecodeError as identifier:
+            return Response({'error': 'Invalid token'}, status=status.HTTP_400_BAD_REQUEST)
+class LoginAPIView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+class RequestPasswordResetEmail(generics.GenericAPIView):
+    serializer_class = ResetPasswordEmailRequestSerializer
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        email = request.data.get('email', '')
+        if User.objects.filter(email=email).exists():
+            user = User.objects.get(email=email)
+            uidb64 = urlsafe_base64_encode(smart_bytes(user.id))
+            token = PasswordResetTokenGenerator().make_token(user)
+            current_site = get_current_site(
+                request=request).domain
+            relativeLink = reverse(
+                'password-reset-confirm', kwargs={'uidb64': uidb64, 'token': token})
+            redirect_url = request.data.get('redirect_url', '')
+            absurl = 'http://'+current_site + relativeLink
+            email_body = 'Hello, \n Use link below to reset your password  \n' + \
+                absurl+"?redirect_url="+redirect_url
+            data = {'email_body': email_body, 'to_email': user.email,
+                    'email_subject': 'Reset your passsword'}
+            Util.send_email(data)
+        return Response({'success': 'We have sent you a link to reset your password'}, status=status.HTTP_200_OK)
+class PasswordTokenCheckAPI(generics.GenericAPIView):
+    serializer_class = SetNewPasswordSerializer
+    def get(self, request, uidb64, token):
+        redirect_url = request.GET.get('redirect_url')
+        try:
+            id = smart_str(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(id=id)
+            if not PasswordResetTokenGenerator().check_token(user, token):
+                if len(redirect_url) > 3:
+                    return CustomRedirect(redirect_url+'?token_valid=False')
+                else:
+                    return CustomRedirect(os.environ.get('FRONTEND_URL', '')+'?token_valid=False')
+            if redirect_url and len(redirect_url) > 3:
+                return CustomRedirect(redirect_url+'?token_valid=True&message=Credentials Valid&uidb64='+uidb64+'&token='+token)
+            else:
+                return CustomRedirect(os.environ.get('FRONTEND_URL', '')+'?token_valid=False')
+        except DjangoUnicodeDecodeError as identifier:
+            try:
+                if not PasswordResetTokenGenerator().check_token(user):
+                    return CustomRedirect(redirect_url+'?token_valid=False')
+            except UnboundLocalError as e:
+                return Response({'error': 'Token is not valid, please request a new one'}, status=status.HTTP_400_BAD_REQUEST)
+class SetNewPasswordAPIView(generics.GenericAPIView):
+    serializer_class = SetNewPasswordSerializer
+    def patch(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response({'success': True, 'message': 'Password reset success'}, status=status.HTTP_200_OK)
+class LogoutAPIView(generics.GenericAPIView):
+    serializer_class = LogoutSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+class SubjectList(APIView):  # get all Subjects
+    permission_classes = (IsAdminOrReadOnly,)
+    def get(self, request, format=None):  # get all Subjects
+        all_Subjects = Subject.objects.all()
+        serializers = SubjectSerializer(all_Subjects, many=True)
+        return Response(serializers.data)
+    def post(self, request, format=None):  # create new Subject
+        serializers = SubjectSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+class SubjectDetail(APIView):  # get, update, delete single Subject
+    permission_classes = (IsAdminOrReadOnly,)
+    def get_object(self, pk):
+        try:
+            return Subject.objects.get(pk=pk)
+        except Subject.DoesNotExist:
+            raise Http404
+    def get(self, request, pk, format=None):  # get Subject
+        subject = self.get_object(pk)
+        serializers = SubjectSerializer(subject)
+        return Response(serializers.data)
+    def put(self, request, pk, format=None):  # update Subject
+        subject = self.get_object(pk)
+        serializers = SubjectSerializer(subject, data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+    def delete(self, request, pk, format=None):  # delete Subject
+        subject = self.get_object(pk)
+        subject.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+class NotesList(APIView):  # get all notes
+    permission_classes = (IsAdminOrReadOnly,)
+    def get(self, request, format=None):  # get all notes
+        all_notes = Notes.objects.all()
+        serializers = NotesSerializer(all_notes, many=True)
+        return Response(serializers.data)
+    def post(self, request, format=None):  # create new note
+        serializers = NotesSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+class NotesDetail(APIView):  # get, update, delete single note
+    permission_classes = (IsAdminOrReadOnly,)
+    def get_object(self, pk):
+        try:
+            return Notes.objects.get(pk=pk)
+        except Notes.DoesNotExist:
+            raise Http404
+    def get(self, request, pk, format=None):  # get note
+        notes = self.get_object(pk)
+        serializers = NotesSerializer(notes)
+        return Response(serializers.data)
+    def put(self, request, pk, format=None):  # update note
+        notes = self.get_object(pk)
+        serializers = NotesSerializer(notes, data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+    def delete(self, request, pk, format=None):  # delete note
+        notes = self.get_object(pk)
+        notes.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
