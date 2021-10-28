fe3a91a4dabeb691575a130f57dfb7465273ac42
Bernard Opiyo <brobernard.254@gmail.com>
2021-10-28 16:52:47 +0300

    add model class Flashcard

--- mycards/models.py
+++ mycards/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+
+class Flashcards(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+    subject = models.CharField(max_length=50)
+    date_joined = models.DateTimeField(auto_now=True)
+    
+
+
+
+
+    def __str__(self):
+        return self.title
